926346298e4f623e5fb12f385b1dd24b1f2ce733
Dhmm15 <diogohoreste@gmail.com>
2024-09-21 10:16:26 -0300

    Add files via upload

---
+++ Aluno.java
@@ -0,0 +1,29 @@
+public class Aluno {
+    private String matricula;
+    private String nome;
+    private double mediaFinal;
+
+    public Aluno(String matricula, String nome, double mediaFinal) {
+        this.matricula = matricula;
+        this.nome = nome;
+        this.mediaFinal = mediaFinal;
+    }
+
+    public String getMatricula() {
+        return this.matricula;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public double getMediaFinal() {
+        return this.mediaFinal;
+    }
+
+    public String toString() {
+        return "\nAluno: " +  "\nnome: '" + nome + '\'' + "\nMatricula: " + matricula + '\''+
+                ", \nmediaFinal: " + mediaFinal +
+                '.';
+    }
+}
